f3079cbfdcd4fde3bf5e60ad07eedf4f16cede06
Davi Banfi <davibanfi1579@outlook.com>
2021-03-30 16:44:41 -0300

    Project Added

---
+++ src/__tests__/finance.test.ts
@@ -0,0 +1,51 @@
+import request from "supertest";
+import { app } from "../app";
+import { createConnection } from "typeorm";
+
+interface IUser {
+  id: string;
+  email: string;
+  name: string;
+  password: string;
+  created_at: Date;
+}
+
+describe("finances", () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it("Should be able to create a finance", async () => {
+    const user_response = await request(app).post("/users").send({
+      email: "user_1@example.com",
+      name: "User Example",
+      password: "passwordexemple",
+    });
+
+    const finance_response = await request(app).post(`/finances/${user_response.body.id}`).send({
+      description: "Dívida com agiota",
+      value: 200,
+    });
+
+    expect(finance_response.status).toBe(201);
+    expect(finance_response.body).toHaveProperty("id");
+  });
+
+  it("Should be able to delete a finance", async () => {
+    const user_response = await request(app).post("/users").send({
+      email: "user_3@example.com",
+      name: "User Example",
+      password: "passwordexemple",
+    });
+
+    const finance_response = await request(app).post(`/finances/${user_response.body.id}`).send({
+      description: "Galo pra Rinha",
+      value: 150,
+    });
+
+    await request(app).delete(`/finances/${user_response.body.id}`);
+
+    expect(finance_response.body.id).toBeUndefined;
+  })
+});
